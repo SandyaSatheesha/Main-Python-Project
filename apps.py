e0d350e63e8ba5c9e8b2040e6b6a1a5708e6bacf
SANDYA S <113702289+SandyaSatheesha@users.noreply.github.com>
2024-03-28 12:26:47 +0530

    empapi

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmpappConfig(AppConfig):
+class EmpapiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "empapp"
+    name = "empapi"
--- views.py
+++ views.py
@@ -1,61 +1,111 @@
-from django.shortcuts import render, redirect
-from .models import Employee
+from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from . models import Employee
+from django.core.serializers import serialize
+from kafka import KafkaProducer
+from json import dumps
 # Create your views here.
-def ems_home(request):
-    employees = Employee.objects.all();
-    emp_data = {
-        'employees': employees
-    }
-    #get all employee data from the table - query set obj (more than one obj)
-    #create a dict obj & add employee data to the dict
-    #send dict object to the emdhome.html througn contect object
-    return render(request, "empapp/emshome.html", context=emp_data)
 
-def create_employee(request):
+def parse_output(emp_json):
+    # convert json to dict
+    emp_meta = json.loads(emp_json)
+    emp_list = []
+    for meta_obj in emp_meta:
+        emp = meta_obj['fields']
+        emp['id'] = meta_obj['pk']
+        emp_list.append(emp)
+
+    emps_json = json.dumps(emp_list)
+    return emps_json
+
+def employee_crud(request, pk=None):
     if request.method == 'GET':
-        return render(request, "empapp/create_employee_form.html")
+        if pk is None:
+            # step 1: get all employee object from db  (query set --> is a database)
+            emp_qs = Employee.objects.all()
+
+            # step 2: convert queryset object  into json
+            emps_json = parse_output(serialize('json', emp_qs))
+            return HttpResponse(emps_json, content_type="application/json")
+        else:
+            # We need to write logic to get employee by id from database
+            #step 1: get emp object from db  based on pk
+            emp = Employee.objects.get(id=pk)
+            #step 2: convert object to python  dict : manually
+            emp_dict = {
+                'id': emp.id,
+                'empid': emp.empid,
+                'name': emp.name,
+                'age': emp.age,
+                'salary': emp.salary,
+                'address': emp.address,
+            }
+            #step 3: convert dict object into json -->json.dumps()
+            emp_json = json.dumps(emp_dict)
+            #resp_dict = {'message': 'GET By ID method got called!'}
+            return HttpResponse(emp_json, content_type="application/json")
     elif request.method == 'POST':
-        empid = request.POST.get('empid')
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        salary = request.POST.get('salary')
-        address = request.POST.get('address')
-        employee = Employee(empid=empid, name=name, age=age, salary=salary, address=address)
-        employee.save()
-        return redirect('/empapp/ems')
-       # emp_dict = {
-       #     "message": "Employee Created Successfully"
-       # }
-      #  return render(request, "empapp/success.html", context=emp_dict)
-
-def update_employee(request, pk):
-    employee_db = Employee.objects.get(id=pk)
-    if request.method == 'GET':
-        emp_dict = {
-            "employee": employee_db
+        # step1 read the input data
+        emp_input_json = request.body
+        # step2
+        emp_input_dict = json.loads(emp_input_json)
+        '''
+        {
+            "empid": 1001,
+            "name": 'sandya',
+            "age": 23,
+            "salary": 50000,
+            "address": 'chennai',
         }
-        return render(request, "empapp/create_employee_form.html", context=emp_dict)
-    elif request.method == 'POST':
-        empid = request.POST.get('empid')
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        salary = request.POST.get('salary')
-        address = request.POST.get('address')
+        '''
 
-        employee_db.empid = empid
-        employee_db.name = name
-        employee_db.age = age
-        employee_db.salary = salary
-        employee_db.address = address
-        employee_db.save()
-        return redirect("/empapp/ems")
+        #step3
+        employee = Employee(empid=emp_input_dict['empid'], name=emp_input_dict['name'], age=emp_input_dict['age'],
+                            salary=emp_input_dict['salary'], address=emp_input_dict['address'])
+        employee.save()
+        send_message(emp_input_dict)
+        resp_dict = {'message': 'Employee Created!!!'}
+        return HttpResponse(json.dumps(resp_dict), content_type="application/json")
 
-def delete_employee(request, pk):
-    employee_db = Employee.objects.get(id=pk)
-    employee_db.delete()
-    return redirect('/empapp/ems')
+    elif request.method == 'PUT':
+        # step 1: REad employee data to be updated --request.body-->json
+        emp_input_json = request.body
+
+        # step 2: convert Json to dict --> json.loads()===> dict
+        emp_input_dict = json.loads(emp_input_json)
+
+        # step 3: get Employee from DB by pk ==>Employee.objects.get(id=pk)
+        emp_db = Employee.objects.get(id=pk)
+
+        # step 4: update Employee DB objects  with input dict data
+        emp_db.empid = emp_input_dict["empid"]
+        emp_db.name = emp_input_dict["name"]
+        emp_db.age = emp_input_dict["age"]
+        emp_db.salary = emp_input_dict["salary"]
+        emp_db.address = emp_input_dict["address"]
+
+        # step 5: call the save method  on top of DB object
+        emp_db.save()
+
+        # step 6: send the response back to the client saying employee db is updates successgully
+        resp_dict = {'message': 'Employee with {} updated successfully!'.format(emp_db.empid)}
+        return HttpResponse(json.dumps(resp_dict), content_type="application/json")
 
 
 
+    elif request.method == 'DELETE':
+        employee = Employee.objects.get(id=pk)
+        empid = employee.empid
+        employee.delete()
+        resp_dict = {'message': 'Employee with {}, Deleted Successfully!!!'.format(empid)}
+        return HttpResponse(json.dumps(resp_dict), content_type="application/json")
 
+def send_message(employee):
+    print('@@@@@@@@ Kafka Producer Sending a message!!!')
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                             value_serializer=lambda x: dumps(x).encode('utf-8'))
 
+    producer.send('EMPLOYEE_TOPIC', value=employee)
+    producer.flush()
+    print('@@@@@@@@ Kafka Producer <<<SENT>>> a message!!!')
